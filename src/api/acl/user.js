9cf61faaf8f635d989603723d6796e1717243e6f
tpaaaaaaa <790195850@qq.com>
2022-12-17 01:00:16 +0800

    Netlify配置文件

--- src/api/acl/user.js
+++ src/api/acl/user.js
@@ -1,91 +1,88 @@
-import request from '@/utils/request';
-
-import qs from 'qs';
-
+import request from "@/utils/request";
 
 // 用户API
+const baseURL = "http://39.98.123.211:8170/admin";
 
 export function login(data) {
-    return request({
-        url: '/acl/index/login',
-        method: 'post',
-        data
-    });
-};
+  return request({
+    url: baseURL + "/acl/index/login",
+    method: "post",
+    data,
+  });
+}
 
 export function getInfo() {
-    return request({
-        url: '/acl/index/info',
-        method: 'get',
-    });
-};
+  return request({
+    url: baseURL + "/acl/index/info",
+    method: "get",
+  });
+}
 
 export function logout() {
-    return request({
-        url: '/acl/index/logout',
-        method: 'post'
-    });
-};
+  return request({
+    url: "/acl/index/logout",
+    method: "post",
+  });
+}
 
-
-const api_name = '/acl/user';
+const api_name = "/acl/user";
 
 export function getPageList(page, limit, username) {
-    return request({
-        url: `${api_name}/${page}/${limit}`,
-        method: 'get',
-        params: { username },
-    });
-};
+  return request({
+    url: `${api_name}/${page}/${limit}`,
+    method: "get",
+    params: { username },
+  });
+}
 
 export function save(data) {
-    console.log(data);
-    return request({
-        url: `${api_name}/save`,
-        method: 'post',
-        data,
-    });
-};
+  console.log(data);
+  return request({
+    url: `${api_name}/save`,
+    method: "post",
+    data,
+  });
+}
 
 export function remove(id) {
-    return request({
-        url: `${api_name}/remove/${id}`,
-        method: 'delete',
-    });
-};
+  return request({
+    url: `${api_name}/remove/${id}`,
+    method: "delete",
+  });
+}
 
 export function detailRole(userId) {
-    return request({
-        url: `${api_name}/toAssign/${userId}`,
-        method: 'get',
-    });
-};
+  return request({
+    url: `${api_name}/toAssign/${userId}`,
+    method: "get",
+  });
+}
 
 // 给用户分配角色
 export function doAssign(userId, roleId) {
-    // console.log('API：', userId, roleId);
-    return request({
-        url: `${api_name}/doAssign`,
-        method: 'post',
-        // params: { userId, roleId: qs.stringify(roleId, { arrayFormat: 'repeat' }) },
-        params: { userId, roleId: roleId.toString() },
-    });
-};
+  // console.log('API：', userId, roleId);
+  return request({
+    url: `${api_name}/doAssign`,
+    method: "post",
+    // params: { userId, roleId: qs.stringify(roleId, { arrayFormat: 'repeat' }) },
+    params: { userId, roleId: roleId.toString() },
+  });
+}
 
 // 批量删除用户
 export const batchRemove = function (idList) {
-    return request({
-        url: `${api_name}/batchRemove`,
-        method: 'delete',
-        data: idList,
-    });
+  return request({
+    url: `${api_name}/batchRemove`,
+    method: "delete",
+    data: idList,
+  });
 };
 
 // 修改用户
 export const update = function (data) {
-    return request({
-        url: `${api_name}/update`,
-        method: 'put',
-        data,
-    });
+  return request({
+    url: `${api_name}/update`,
+    method: "put",
+    data,
+  });
 };
--- src/api/product/attr.js
+++ src/api/product/attr.js
@@ -1,28 +1,32 @@
 // 平台属性管理模块
-import request from '@/utils/request'
+import request from "@/utils/request";
 
 // 一级分类         GET /admin/product/getCategory1
-export const reqCategory1List = () => request({
-  url: '/product/getCategory1',
-  method: 'get'
-})
+export const reqCategory1List = () =>
+  request({
+    url: "/product/getCategory1",
+    method: "get",
+  });
 
 // 二级分类         GET /admin/product/getCategory2/{category1Id}
-export const reqCategory2List = (category1Id) => request({
-  url: `/product/getCategory2/${category1Id}`,
-  method: 'get'
-})
+export const reqCategory2List = (category1Id) =>
+  request({
+    url: `/product/getCategory2/${category1Id}`,
+    method: "get",
+  });
 // 三级分类         GET /admin/product/getCategory3/{category2Id}
-export const reqCategory3List = (category2Id) => request({
-  url: `/product/getCategory3/${category2Id}`,
-  method: 'get'
-})
+export const reqCategory3List = (category2Id) =>
+  request({
+    url: `/product/getCategory3/${category2Id}`,
+    method: "get",
+  });
 
 // 获取对应属性    GET /admin/product/attrInfoList/{category1Id}/{category2Id}/{category3Id}
-export const reqAttrList = (category1Id, category2Id, category3Id) => request({
-  url: `/product/attrInfoList/${category1Id}/${category2Id}/${category3Id}`,
-  method: 'get'
-})
+export const reqAttrList = (category1Id, category2Id, category3Id) =>
+  request({
+    url: `/product/attrInfoList/${category1Id}/${category2Id}/${category3Id}`,
+    method: "get",
+  });
 
 // 添加属性与属性值  POST /admin/product/saveAttrInfo
 /*
@@ -40,8 +44,9 @@ export const reqAttrList = (category1Id, category2Id, category3Id) => request({
   "id": 0
 }
 */
-export const reqAddOrUpdateAttr = (data) => request({
-  url: '/product/saveAttrInfo',
-  method: 'post',
-  data
-})
+export const reqAddOrUpdateAttr = (data) =>
+  request({
+    url: "/product/saveAttrInfo",
+    method: "post",
+    data,
+  });
